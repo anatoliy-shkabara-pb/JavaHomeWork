efadd9d491d04782bf43de726c0c2738214bbeec
Anatoliy Shkabara <anatolij.shkabara@privatbank.ua>
2021-12-29 11:35:50 +0200

    init hw12

---
+++ src/com/pb/shkabara/hw12/LocalDateTimeDeserializer.java
@@ -0,0 +1,24 @@
+package com.pb.shkabara.hw12;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeDeserializer extends StdDeserializer<LocalDateTime> {
+
+    private static final long serialVersionUID = 1L;
+
+    protected LocalDateTimeDeserializer() {
+        super(LocalDateTime.class);
+    }
+
+
+    @Override
+    public LocalDateTime deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+        return LocalDateTime.parse(jp.readValueAs(String.class));
+    }
+}
